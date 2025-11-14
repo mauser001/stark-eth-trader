76e925b36f62b77647dce62f3467b4f90a76421d
Alexander <mauser001@hotmail.com>
2025-11-14 16:57:55 +0100

    reporting compare trades based on matched trades

--- src/reporting.ts
+++ src/reporting.ts
@@ -2,21 +2,24 @@ import { formatEther } from "ethers";
 import { getTransactionData } from "./transactions";
 import { BigNumber } from "@ethersproject/bignumber";
 
-function printBalance(label: string, eth: string = '', strk: string = '') {
-    console.log(`${label} in wei: Eth: ${eth} |  Strk: ${strk}`)
-    console.log(`${label} formated: Eth: ${formatEther(eth)} | Strk: ${formatEther(strk)}`)
+function printBalance(label: string, eth: string = '', strk: string = '', date: string = '') {
+    console.log(`${label} in wei: Eth: ${eth} |  Strk: ${strk} | date: ${date}`)
+    console.log(`${label} formated: Eth: ${formatEther(eth)} | Strk: ${formatEther(strk)} | date: ${date}`)
 }
 
 async function analyseTades() {
-    const transactions = await getTransactionData()
+    let transactions = await getTransactionData()
     if (transactions?.length < 2) {
         console.log('no transaction done')
     }
 
+    const firstMatchedTradeIndex = transactions.findIndex(({ matchedBy }, i) => i > 0 && !!matchedBy)
+    console.log('Trades unmatched: ', firstMatchedTradeIndex - 1, ' of ', transactions.length)
+    transactions = transactions.slice(firstMatchedTradeIndex - 1)
     const firstTrade = transactions[0]
-    printBalance('Initial Balance', firstTrade.balanceEth, firstTrade.balanceStrk)
+    printBalance('Initial Balance', firstTrade.balanceEth, firstTrade.balanceStrk, new Date(firstTrade.timestamp).toDateString())
     const lastTrade = transactions[transactions.length - 1]
-    printBalance('Last Balance', lastTrade.balanceEth, lastTrade.balanceStrk)
+    printBalance('Last Balance', lastTrade.balanceEth, lastTrade.balanceStrk, new Date(lastTrade.timestamp).toDateString())
 
     const firstBalanceEth = BigNumber.from(firstTrade.balanceEth)
     const firstBalanceStrk = BigNumber.from(firstTrade.balanceStrk)
@@ -24,23 +27,23 @@ async function analyseTades() {
     const lastBalanceStrk = BigNumber.from(lastTrade.balanceStrk)
 
     if (firstBalanceEth.lt(lastBalanceEth)) {
-        console.log(`We have more ${formatEther(lastBalanceEth.sub(firstBalanceEth).toString())} Eth `)
+        console.log(`We have more ${formatEther(lastBalanceEth.sub(firstBalanceEth).toString())} Eth  on matched trades`)
     } else {
-        console.log(`We have less ${formatEther(firstBalanceEth.sub(lastBalanceEth).toString())} Eth `)
+        console.log(`We have less ${formatEther(firstBalanceEth.sub(lastBalanceEth).toString())} Eth  on matched trades`)
     }
     if (firstBalanceStrk.lt(lastBalanceStrk)) {
-        console.log(`We have more ${formatEther(lastBalanceStrk.sub(firstBalanceStrk).toString())} Strk `)
+        console.log(`We have more ${formatEther(lastBalanceStrk.sub(firstBalanceStrk).toString())} Strk on matched trades`)
     } else {
-        console.log(`We have less ${formatEther(firstBalanceStrk.sub(lastBalanceStrk).toString())} Strk `)
+        console.log(`We have less ${formatEther(firstBalanceStrk.sub(lastBalanceStrk).toString())} Strk  on matched trades`)
     }
-    const totalBalanceStart = firstBalanceEth.add(firstBalanceEth)
     const lastTradeEth = BigNumber.from(lastTrade.sell === 'eth' ? lastTrade.sellAmount : lastTrade.buyAmount)
     const lastTradeStrk = BigNumber.from(lastTrade.sell === 'strk' ? lastTrade.sellAmount : lastTrade.buyAmount)
+    const totalBalanceStart = firstBalanceEth.add(firstBalanceStrk.mul(lastTradeEth).div(lastTradeStrk))
     const totalBalanceEnd = lastBalanceEth.add(lastBalanceStrk.mul(lastTradeEth).div(lastTradeStrk))
     if (totalBalanceStart.lt(totalBalanceEnd)) {
-        console.log(`In total we have more ${formatEther(totalBalanceEnd.sub(totalBalanceStart).toString())} Eth`)
+        console.log(`In total we have more ${formatEther(totalBalanceEnd.sub(totalBalanceStart).toString())} Eth on matched trades`)
     } else {
-        console.log(`In total we have less ${formatEther(totalBalanceStart.sub(totalBalanceEnd).toString())} Eth `)
+        console.log(`In total we have less ${formatEther(totalBalanceStart.sub(totalBalanceEnd).toString())} Eth on matched trades`)
     }
     const count = {
         ethSell: 0,
@@ -49,7 +52,7 @@ async function analyseTades() {
         strkMatched: 0,
     }
     transactions.slice(1).forEach(t => {
-        if (t.sell === 'eth') {
+        if (t.sell === 'eth' && !t.matchedBy) {
             const ratio = BigNumber.from(t.buyAmount).div(BigNumber.from(t.sellAmount))
             console.log('Ratio: ', ratio.toString(), new Date(t.timestamp).toDateString())
         }
